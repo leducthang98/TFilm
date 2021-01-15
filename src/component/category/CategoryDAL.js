75bde928a8089f910b2e907e34599e8c29444e9f
LeDucThang <leducthang98@gmail.com>
2021-01-15 23:00:25 +0700

    ok

--- src/component/category/CategoryDAL.js
+++ src/component/category/CategoryDAL.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import moment from 'moment';
 
 export const getAllCategoryDAL = async () => {
-    let sql = 'select * from category';
+    let sql = 'select id, category_name as name from category';
     const result = await dbUtil.query(sql, []);
     return result;
 }

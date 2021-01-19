25a9e6bdcbbb5ba6e755ab2b0b958e41247819ab
LeDucThang <leducthang98@gmail.com>
2021-01-20 00:42:09 +0700

    ok

--- src/component/movie/MovieController.js
+++ src/component/movie/MovieController.js
@@ -5,12 +5,12 @@ import { createMovieDAL, getAllMovieDAL, getCategoriesOfMovieDAL, getSeasonOfMov
 
 export const createMovie = async (req, res, next) => {
     let id = uuidv4();
-    const response = await createMovieDAL(id, req.body)
+    const response = await createMovieDAL(id, req.query.movieType, req.body)
     res.send(commonResponse(response))
 }
 
 export const getAllMovie = async (req, res, next) => {
-    const response = await getAllMovieDAL(req.query.searchData,req.query.category, req?.limit, req?.offset, req?.page, req?.size)
+    const response = await getAllMovieDAL(req.query.searchData, req.query.category, req.query.movieType, req?.limit, req?.offset, req?.page, req?.size)
     res.send(commonResponse(response))
 }
 
--- src/component/movie/MovieDAL.js
+++ src/component/movie/MovieDAL.js
@@ -1,13 +1,16 @@
 import * as dbUtil from '../../util/Database';
 import { v4 as uuidv4 } from 'uuid';
 import moment from 'moment';
+// default: series Movies
+// single: single Movies
 
-export const createMovieDAL = async (id, data) => {
+export const createMovieDAL = async (id, movieType, data) => {
+    // 1 : series, 0: single
     const transaction = await dbUtil.beginTransaction();
     try {
         let created_at = moment().format('YYYY-MM-DD hh:mm:ss');
-        const sql = 'insert into `movie` (id, name, description, image, created_at, updated_at, deleted, director_id, link_movie) values (?,?,?,?,?,?,?,?,?)';
-        await dbUtil.executeInTransaction(sql, [id, data.name, data.description, data.image, created_at, created_at, 0, data.directorId, data.linkMovie]).catch(err => {
+        const sql = 'insert into `movie` (id, name, description, image, created_at, updated_at, deleted, director_id, link_trailer,single_episode) values (?,?,?,?,?,?,?,?,?,?)';
+        await dbUtil.executeInTransaction(sql, [id, data.name, data.description, data.image, created_at, created_at, 0, data.directorId, data.linkTrailer, movieType === 'series' ? 2 : 1]).catch(err => {
             throw err;
         })
         for (let i = 0; i < data.category.length; i++) {
@@ -18,6 +21,23 @@ export const createMovieDAL = async (id, data) => {
                 throw err;
             })
         }
+        if (movieType === 'single') {
+            console.log('SINGLE_MOVIE')
+            let seasonId = uuidv4();
+            let seasonName = id + '_' + data.name + '_season';
+            let addSeasonForSingleMovieSql = 'insert into season (id,movie_id,name,image,description,deleted,created_at,updated_at) values (?,?,?,?,?,?,?,?)';
+            await dbUtil.executeInTransaction(addSeasonForSingleMovieSql, [seasonId, id, seasonName, data.image, data.description, 0, created_at, created_at]).catch(err => {
+                throw err;
+            })
+            // tao eps
+            let episodeId = uuidv4();
+            let epsName = id + '_' + data.name + '_episode';
+            let addEpsForSingleMovieSql = 'insert into episode (id, season_id, name,image,description,deleted,created_at,updated_at,url,single_episode) values (?,?,?,?,?,?,?,?,?,?)';
+            await dbUtil.executeInTransaction(addEpsForSingleMovieSql, [episodeId, seasonId, epsName, data.image, data.description, 0, created_at, created_at, data.linkFilm, 1]).catch(err => {
+                throw err;
+            })
+        }
+
         await dbUtil.commitTransaction(transaction);
         return true;
     } catch (e) {
@@ -26,12 +46,25 @@ export const createMovieDAL = async (id, data) => {
     }
 }
 
-export const getAllMovieDAL = async (searchData, category, limit, offset, page, size) => {
+export const getAllSingleMovieDAL = async () => {
+
+}
+
+export const getAllMovieDAL = async (searchData, category, movieType, limit, offset, page, size) => {
     let sql = 'select m.*, mc.category_id,c.category_name from `movie` m LEFT JOIN `movie_category` mc on m.id = mc.movie_id INNER JOIN `category` c on c.id = mc.category_id where m.deleted = 0';
     let params = [];
     if (category) {
         sql += ' and mc.category_id = ?';
         params.push(category)
+    } else {
+        sql = 'select * from movie m where m.deleted = 0';
+    }
+    if (movieType) {
+        if (movieType == 'series') {
+            sql += ' and single_episode = 2';
+        } else {
+            sql += ' and single_episode = 1';
+        }
     }
     if (searchData) {
         sql += ' and lower(name) like ?';
@@ -40,7 +73,7 @@ export const getAllMovieDAL = async (searchData, category, limit, offset, page, 
     params.push(limit)
     params.push(offset)
     sql += ' limit ? offset ?';
-    const result = await dbUtil.query(sql, params);
+    let result = await dbUtil.query(sql, params);
 
     // count
     let countSql = 'select count(m.id) as count from `movie` m LEFT JOIN `movie_category` mc on m.id = mc.movie_id INNER JOIN `category` c on c.id = mc.category_id where m.deleted = 0';
@@ -48,7 +81,18 @@ export const getAllMovieDAL = async (searchData, category, limit, offset, page, 
     if (category) {
         countSql += ' and mc.category_id = ?';
         paramsCount.push(category)
+    } else {
+        countSql = 'select count(m.id) as count from movie m where m.deleted = 0';
     }
+
+    if (movieType) {
+        if (movieType = 'series') {
+            countSql += ' and single_episode = 2';
+        } else {
+            countSql += ' and single_episode = 1';
+        }
+    }
+
     if (searchData) {
         countSql += ' and lower(name) like ?';
         paramsCount.push('%' + searchData.toLowerCase() + '%');
@@ -62,6 +106,18 @@ export const getAllMovieDAL = async (searchData, category, limit, offset, page, 
         totalPage = (count / limit) + 1
     }
 
+    for (let i = 0; i < result.length; i++) {
+        let movieType = 'unknown'
+        if (result[i].single_episode == 1) {
+            movieType = 'single'
+        } else if (result[i].single_episode == 2) {
+            movieType = 'series'
+        }
+        result[i] = {
+            ...result[i],
+            movie_type: movieType
+        }
+    }
     const finalResult = {
         data: result,
         pagination: {
@@ -110,3 +166,4 @@ export const updateCategoryForMovieDAL = async (movieId, categoriesId) => {
         return e;
     }
 }
+
